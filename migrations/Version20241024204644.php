15e29ae9b468e761d170411b52c28d5c26ac0489
perezadria00 <pl2023275@365.stucom.com>
2024-10-24 23:02:52 +0200

    server ddbb

---
+++ migrations/Version20241024204644.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241024204644 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE nurse (id INT AUTO_INCREMENT NOT NULL, user VARCHAR(30) NOT NULL, password VARCHAR(60) NOT NULL, name VARCHAR(30) DEFAULT NULL, surname VARCHAR(30) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci`');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE nurse');
+    }
+}
