42d131f1cdbe58be8bb72d795cd4ddfe4324c69d
perezadria00 <pl2023275@365.stucom.com>
2024-10-24 21:56:29 +0200

    search by name

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -13,5 +13,26 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/nurse')]
 class NurseController extends AbstractController
 {
-    
+    #[Route('/name/{name}/{surname}', name: 'nurses_names', methods: ['GET'])]
+    public function findByNameAndSurname(NurseRepository $nurseRepository, string $name, string $surname): JsonResponse
+    {
+        $foundNurses = $nurseRepository->findByNameAndSurname($name, $surname);
+
+        if (empty($foundNurses)) {
+            return new JsonResponse(
+                ['message' => 'No nurses found with the given name: ' . $name . ' and surname: ' . $surname],
+                JsonResponse::HTTP_NOT_FOUND
+            );
+        }
+
+        $nurseNames = array_map(function (Nurse $nurse) {
+            return $nurse->getName() . ' ' . $nurse->getSurname();
+        }, $foundNurses);
+
+        return new JsonResponse(
+            ['found_nurses' => $nurseNames],
+            JsonResponse::HTTP_OK
+        );
+    }
+
 }
