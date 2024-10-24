9bc3ddb74e2ce6613318615f74fdf7bd94907183
perezadria00 <pl2023275@365.stucom.com>
2024-10-24 22:07:32 +0200

    getAll

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -13,5 +13,23 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/nurse')]
 class NurseController extends AbstractController
 {
+    #[Route('/index', name: 'nurse_index', methods: ['GET'])]
+    public function getAll(NurseRepository $nurseRepository): JsonResponse
+    {
+        // Obtener todos los enfermeros de la base de datos
+        $nurses = $nurseRepository->findAll();
+    
+        $nursesData = [];
+        foreach ($nurses as $nurse) {
+            $nursesData[] = [
+                'id' => $nurse->getId(),
+                'name' => $nurse->getName(),
+                'surname' => $nurse->getSurname(),
+            ];
+        }
+    
+        // Retornar los datos en formato JSON
+        return new JsonResponse($nursesData, JsonResponse::HTTP_OK);
+    }
     
 }
