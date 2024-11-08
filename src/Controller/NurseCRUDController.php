30b9add7840f1ff49482b04f93a2f68959fa6ba4
perezadria00 <pl2023275@365.stucom.com>
2024-11-08 15:42:27 +0100

    fix#22

--- src/Controller/NurseCRUDController.php
+++ src/Controller/NurseCRUDController.php
@@ -12,49 +12,98 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-#[Route('/nurse/crud')]
+#[Route('/nurse')]
 final class NurseCRUDController extends AbstractController
 {
     //CRUD para entidad Nurse
 
     //devuelve todos los usuarios
     #[Route(name: 'app_nurse_c_r_u_d_index', methods: ['GET'])]
-    public function index(NurseRepository $nurseRepository): Response
+    public function index(NurseRepository $nurseRepository): JsonResponse
     {
-        return $this->render('nurse_crud/index.html.twig', [
-            'nurses' => $nurseRepository->findAll(),
-        ]);
+        $nurses = $nurseRepository->findAll();
+        $nursesData = array_map(function ($nurse) {
+            return [
+                'name' => $nurse->getName(),
+                'surname' => $nurse->getSurname(),
+                'id' => $nurse->getId(),
+                'user' => $nurse->getUser(),
+                'password' => $nurse->getPassword(),
+            ];
+        }, $nurses);
+    
+        return $this->json(['nurses' => $nursesData], Response::HTTP_OK);
     }
-
-
-    //añade un usuario
+    
+    // Crear un enfermero
     #[Route('/new', name: 'app_nurse_c_r_u_d_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $nurse = new Nurse();
         $form = $this->createForm(NurseType::class, $nurse);
         $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($nurse);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_nurse_c_r_u_d_index', [], Response::HTTP_SEE_OTHER);
+    
+        // Procesar el formulario si la solicitud es POST
+        if ($request->isMethod('POST')) {
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager->persist($nurse);
+                $entityManager->flush();
+    
+                // Devolver los datos del nuevo usuario en formato JSON con 201 Created
+                return $this->json([
+                    'status' => 'success',
+                    'nurse' => [
+                        'id' => $nurse->getId(),
+                        'name' => $nurse->getName(),
+                        'surname' => $nurse->getSurname(),
+                        'user' => $nurse->getUser(),
+                        'password' => $nurse->getPassword()
+                       
+                    ]
+                ], Response::HTTP_CREATED);
+            }
+    
+            // Si hay errores de validación, devolverlos en formato JSON con 400 Bad Request
+            $errors = [];
+            foreach ($form->getErrors(true) as $error) {
+                $errors[] = $error->getMessage();
+            }
+    
+            return $this->json([
+                'status' => 'error',
+                'errors' => $errors
+            ], Response::HTTP_BAD_REQUEST);
         }
-
+    
+        // Si la solicitud es GET, renderizar el formulario en HTML
         return $this->render('nurse_crud/new.html.twig', [
             'nurse' => $nurse,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
-    //devuelve un usuario dado un id
+    
+    // Devuelve un usuario dado un id
     #[Route('/{id}', name: 'app_nurse_c_r_u_d_show', methods: ['GET'])]
-    public function show(Nurse $nurse): Response
+    public function show(int $id, NurseRepository $nurseRepository): Response
     {
-        return $this->render('nurse_crud/show.html.twig', [
-            'nurse' => $nurse,
-        ]);
+        $nurse = $nurseRepository->findByID($id);
+
+        // Si no se encuentra, devolver una respuesta 404
+        if (!$nurse) {
+            return $this->json(['error' => 'Nurse not found'], 404);
+        }
+
+        // Si se encuentra, devuelve los datos del enfermero
+        $nurseData = [
+            'name' => $nurse->getName(),
+            'surname' => $nurse->getSurname(),
+            'id' => $nurse->getId(),
+            'user' => $nurse->getUser(),
+            'password' => $nurse->getPassword()
+        ];
+
+        return $this->json($nurseData, 200);
     }
 
     //actualiza un usuario dado un id
@@ -72,7 +121,7 @@ final class NurseCRUDController extends AbstractController
 
         return $this->render('nurse_crud/edit.html.twig', [
             'nurse' => $nurse,
-            'form' => $form,
+            'form' => $form,    
         ]);
     }
 
@@ -80,7 +129,7 @@ final class NurseCRUDController extends AbstractController
     #[Route('/{id}', name: 'app_nurse_c_r_u_d_delete', methods: ['POST'])]
     public function delete(Request $request, Nurse $nurse, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$nurse->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $nurse->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($nurse);
             $entityManager->flush();
         }
